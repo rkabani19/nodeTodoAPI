b1de243f6d5834948451d3995cd53f5d9f4692da
Rohail Kabani <rohail.kabani@gmail.com>
2017-09-26 10:49:35 -0400

    Init commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,32 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if(err) {
+    return console.log('Unable to connect to MogoDB server.');
+  }
+  console.log('Connected to the MogoDB server');
+
+  // db.collection('Todos').find({
+  //     _id: new ObjectID('59c13939d8aa45a848b15e9b'
+  //   )}).toArray().then((docs) => {
+  //   console.log('Todos');
+  //   console.log(JSON.stringify(docs, undefined, 2));
+  // }, (err) => {
+  //   console.log("Unable to fetch todos", err)
+  // })
+
+  // db.collection('Todos').find().count().then((count) => {
+  //   console.log('Todos count: ' + count);
+  // }, (err) => {
+  //   console.log("Unable to fetch todos", err)
+  // })
+
+  db.collection('Users').find({name: 'Rohail Kabani'}).count().then((count) => {
+    console.log('Todos count for name Rohail Kabani is ' + count);
+  }, (err) => {
+    console.log("Unable to fetch todos for Rohail", err)
+  })
+
+
+  // db.close();
+});
