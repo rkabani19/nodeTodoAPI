dc5f2664a137fc6766d22ace8dcdf03711259585
Rohail Kabani <rohail.kabani@gmail.com>
2017-11-01 13:06:35 -0400

    minor syntax fixes

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,13 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+let connection = mongoose.connect('mongodb://localhost:27017/TodoApp', {
+  useMongoClient: true
+}).then(db => {
+  console.log('MongoDB is connected')
+}).catch(error => {
+  console.log(error)
+})
 
 module.exports = {
   mongoose: mongoose
--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var {ObjectID} = require('mongoDB');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todos');
